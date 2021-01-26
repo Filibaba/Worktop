067bdbb6461f196f0ab33453a8dcf273d505b99a
Simon Ljungberg <hi@iamsim.me>
2021-01-26 16:24:31 +0100

    Add public initializer to ConfirmState

--- Sources/Worktop/ConfirmState.swift
+++ Sources/Worktop/ConfirmState.swift
@@ -1,8 +1,18 @@
 /// The "confirm state" wraps information to show an alert modal.
 public struct ConfirmState<Action: Equatable>: Equatable {
+
   public var title: String
   public var message: String?
   public var confirmTitle: String
   public var confirmAction: Action
   public var cancelAction: Action
+
+  public init(title: String, message: String? = nil, confirmTitle: String, confirmAction: Action, cancelAction: Action) {
+    self.title = title
+    self.message = message
+    self.confirmTitle = confirmTitle
+    self.confirmAction = confirmAction
+    self.cancelAction = cancelAction
+  }
+
 }
