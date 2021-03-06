6991cdf5e73e0ae196af4960ebf5b83a809c1299
Simon Ljungberg <hi@iamsim.me>
2021-03-06 22:36:20 +0100

    Remove mac support again

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ let package = Package(
   name: "Worktop",
   platforms: [
     .iOS(.v14),
-    .macOS(.v11)
   ],
   products: [
     .library(name: "Worktop", targets: ["Worktop"]),
@@ -15,8 +14,8 @@ let package = Package(
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture/", from: "0.15.0"),
   ],
   targets: [
-    .target(name: "Worktop",dependencies: [
+    .target(name: "Worktop", dependencies: [
       .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-    ])
+    ]),
   ]
 )
