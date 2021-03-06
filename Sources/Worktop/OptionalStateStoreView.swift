1dad06d2cd7affadb605da6f426c6231d2ab60fd
Simon Ljungberg <hi@iamsim.me>
2021-03-06 22:02:22 +0100

    Add a state store view with support for optional store

---
+++ Sources/Worktop/OptionalStateStoreView.swift
@@ -0,0 +1,71 @@
+import Combine
+import ComposableArchitecture
+import UIKit
+
+/// Convenience class for views that are powered by state stores.
+open class OptionalStateStoreView<State: Equatable, Action>: UIView {
+
+  public typealias Store = ComposableArchitecture.Store<State, Action>
+  public typealias ViewStore = ComposableArchitecture.ViewStore<State, Action>
+
+  // MARK: Properties
+
+  /// The store powering the view.
+  private(set) public var store: Store?
+
+  /// The view store wrapping the store for the view.
+  private(set) public var viewStore: ViewStore?
+
+  /// Keeps track of subscriptions.
+  open var cancellables: Set<AnyCancellable> = []
+
+  // MARK: Initialization
+
+  /// Creates a new store view with the given store.
+  ///
+  /// - Parameter frame: The initial frame of the view.
+  /// - Parameter store: The store to use with the view.
+  ///
+  /// - Returns: A new view controller.
+  public init(frame: CGRect = .zero, store: Store? = nil) {
+    self.store = store
+    self.viewStore = store.map(ViewStore.init)
+    super.init(frame: frame)
+    configureView()
+    if let store = store {
+      configureStateObservation(on: viewStore)
+    }
+  }
+
+  @available(*, unavailable) public required init?(coder: NSCoder) {
+    fatalError("Not implemented")
+  }
+
+  // MARK: Setting a store
+
+  /// Sets a store for the view and resets cancellables.
+  ///
+  /// - Parameter store: A new stor, or nil.
+  public func setStore(_ store: Store?) {
+    cancellables.removeAll()
+    self.store = store
+    if let store = store {
+      let viewStore = ViewStore(store)
+      self.viewStore = viewStore
+      configureStateObservation(on: viewStore)
+    } else {
+      self.viewStore = nil
+    }
+  }
+
+  // MARK: Subclass API
+
+  /// Override this method to configure state observation.
+  ///
+  /// - Parameter viewStore: The view store to observe.
+  open func configureStateObservation(on viewStore: ViewStore) { }
+
+  /// Override this method to setup views when a cell is created.
+  open func configureView() { }
+
+}
