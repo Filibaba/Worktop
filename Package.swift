b9cb7f8538ba1061b7bbb8fc6beca521b14da0ba
Simon Ljungberg <hi@iamsim.me>
2021-04-13 15:53:25 +0200

    Update composable architecture to 0.17.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     .library(name: "Worktop", targets: ["Worktop"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture/", from: "0.15.0"),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture/", from: "0.17.0"),
   ],
   targets: [
     .target(name: "Worktop", dependencies: [
